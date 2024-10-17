de94391680ecd78fe7943a6eefd316abe690428a
Ray <60537896+ruiyushen@users.noreply.github.com>
2024-10-18 00:10:32 +0800

    Update py2mlir.py

--- py2mlir.py
+++ py2mlir.py
@@ -1,8 +1,8 @@
 #   Steps:
 #   
 #   git clone https://github.com/llvm/llvm-project.git
-#   git checkout 8e0daabe97cf5e73402bcb4c3e54b3583199ba8f
 #   cd llvm-project
+#   git checkout 8e0daabe97cf5e73402bcb4c3e54b3583199ba8f
 #   cmake -S llvm -B build -G Ninja -DLLVM_ENABLE_PROJECTS="mlir"    \
 #   -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="host"          \
 #   -DCMAKE_BUILD_TYPE=Release -DPython3_EXECUTABLE=$(which python3) \
