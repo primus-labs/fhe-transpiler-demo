dff01d9a9a3c8e702c1cc53c4b51ff858101cba3
Ray <60537896+ruiyushen@users.noreply.github.com>
2024-11-09 17:38:57 +0800

    Update boxblur.py

--- benchmarks/boxblur/boxblur.py
+++ benchmarks/boxblur/boxblur.py
@@ -1,9 +1,9 @@
-def encryptedBoxBlur_8x8(arg0:list[float,64], arg1:list[float, 64]):
-    for x in range(8):
-        for y in range(8):
+def encryptedBoxBlur_64x64(arg0:list[float,4096], arg1:list[float, 4096]):
+    for x in range(64):
+        for y in range(64):
             value = 0.0
             for j in range(3):
                 for i in range(3):
-                    value += arg0[((x+i-1)*8+y+j-1)%64 ]
-            arg1[(8*x+y)%64] = value
+                    value += arg0[((x+i-1)*64+y+j-1)%4096 ]
+            arg1[(64*x+y)%4096] = value
     return arg1
