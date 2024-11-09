cd3d2d1f5d158a2f6c897077ae78e02443f52431
Ray <60537896+ruiyushen@users.noreply.github.com>
2024-11-09 17:39:21 +0800

    Update robertscross.py

--- benchmarks/robertscross/robertscross.py
+++ benchmarks/robertscross/robertscross.py
@@ -1,11 +1,11 @@
-def encryptedRobertsCross_32x32(img:list[float, 1024], output:list[float, 1024]):
-    for x in range(32):
-        for y in range(32):
-            val1 = img[((x - 1) * 32 + (y - 1)) % 1024]
-            val2 = img[(x * 32 + y) % 1024]
-            val3 = img[((x - 1) * 32 + y) % 1024]
-            val4 = img[(x * 32 + (y - 1)) % 1024]
+def encryptedRobertsCross_64x64(img:list[float, 4096], output:list[float, 4096]):
+    for x in range(64):
+        for y in range(64):
+            val1 = img[((x - 1) * 64 + (y - 1)) % 4096]
+            val2 = img[(x * 64 + y) % 4096]
+            val3 = img[((x - 1) * 64 + y) % 4096]
+            val4 = img[(x * 64 + (y - 1)) % 4096]
             diff1 = (val1 - val2)*(val1 - val2)
             diff2 = (val3 - val4)*(val3 - val4)
-            output[(x * 32 + y) % 1024] = diff1 + diff2
+            output[(x * 64 + y) % 4096] = diff1 + diff2
     return output
