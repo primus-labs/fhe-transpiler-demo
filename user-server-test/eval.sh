556d145a43b57ac104576abd0572fa1cc8942db0
chenyuanfeng <yuanfeng@padolabs.org>
2025-01-13 16:34:12 +0800

    refine script files

--- user-server-test/eval.sh
+++ user-server-test/eval.sh
@@ -3,4 +3,7 @@
 export PYTHONPATH=~/mylibs/llvm19/python_packages/mlir_core:${PYTHONPATH}
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/mylibs/lib
 
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+cd "$SCRIPT_DIR/.."
+
 python user-server-test/server-eval.py
